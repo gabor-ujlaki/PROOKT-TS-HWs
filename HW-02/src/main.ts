90d4f5806c83af5035f3cef02836676142d69ccb
gabor-ujlaki <gabor.ujlaki@gmail.com>
2026-09-02 21:24:38 +0200

    Hw-02: change main program

--- HW-02/src/main.ts
+++ HW-02/src/main.ts
@@ -5,6 +5,7 @@ import { Inventory } from './classes/Inventory';
 /* TESZT */
 const prod1= new Product('PROD001', 'TV', 150000, Currency.HUF, UoM.DB);
 const prod2 = new Product('PROD002', 'Video', 50000, Currency.HUF, UoM.DB);
+const prod3 = new Product('PROD003', 'Mikró', 135.49, Currency.EUR, UoM.DB);
 console.log(`product id: ${prod1.id}`);
 console.log(`produst price: ${prod1.price}`);
 console.log('product details:', prod1.getProductDetails());
@@ -14,4 +15,25 @@ console.log('product details:', prod1.getProductDetails());
 prod1.printProduct();
 
 const wh1 = new Inventory('WH001', 'Raktár 01');
-wh1.assignItem({product: prod1, qty: 2});
+wh1.assignItem(prod1);
+wh1.assignItem(prod1);
+wh1.assignItem(prod2);
+wh1.assignItem(prod3);
+wh1.printStock();
+
+wh1.addItemQty(prod1, 2);
+wh1.addItemQty(prod2, 3);
+wh1.addItemQty(prod3, 6);
+wh1.printStock();
+
+wh1.substItemQty(prod3, 2);
+wh1.printStock();
+
+wh1.substItemQty(prod3, 4);
+wh1.substItemQty(prod3, 1);
+
+wh1.searchStockItem('PROD001');
+wh1.searchStockItem('i');
+
+wh1.removeItem('PROD003');
+wh1.printStock();
